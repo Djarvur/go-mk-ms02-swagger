2510d94ce70f585361277c83ff3ede30e9c9a821
Alex Chistyakov <alexclear@gmail.com>
2017-06-18 12:48:09 +0300

    Add swagger definition and generated code

---
+++ restapi/doc.go
@@ -0,0 +1,24 @@
+/*Package restapi testauth
+
+This is a simple auth service
+
+
+    Schemes:
+      http
+    Host: localhost:8080
+    BasePath: /testauth
+    Version: 0.0.1
+    License: MIT
+    Contact: <onokonem@gmail.com>
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
